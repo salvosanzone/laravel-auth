adbe16f0eb4f3482e8a5670e7804351befd021e6
salvosanzone <salvosansone.ss@gmail.com>
2022-02-02 17:20:49 +0100

    inserimento vista posts.admin.index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
                 <a class="navbar-brand" href="{{ route('home') }}">
                     Vail al sito pubblico
                 </a>
+                <a href="{{ route('admin.posts.index') }}">Elenco Post</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,8 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function(){
         Route::get('/', 'HomeController@index')->name('index');
+
+        Route::resource('/posts', 'PostController');
     });
+
+
